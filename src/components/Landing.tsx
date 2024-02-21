2b4cad88ba29bd5ceed4e8ccb80d411870dca720
Sharad Jadhav <60263979+jadhavsharad@users.noreply.github.com>
2024-02-21 13:37:15 +0530

    updated

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -8,7 +8,7 @@ const Landing = () => {
       <div>
         <NavBar />
       </div>
-      <div className="__hero flex items-center justify-between min-h-[100dvh] w-full overflow-hidden">
+      <div className="__hero flex items-center justify-between min-h-[100lvh] w-full overflow-hidden">
         <div className="flex flex-col w-full h-full md:py-36 lg:py-0 justify-center items-center gap-14">
           <TypeAnimation className="text-white text-3xl break-before-all lg:text-5xl font-Orbitron font-bold uppercase " sequence={['DATAVERSE', 1000, 'BY', 500, 'Data Science Club', 1000]} wrapper="h1" speed={1} repeat={Infinity}></TypeAnimation>
           <p className="text-white font-Oxanium px-6 lg:px-0 text-sm lg:text-lg max-w-screen-lg text-center leading-relaxed">Welcome, Data enthusiasts! Prepare to embark on a transformative journey of ideation, innovation, and collaboration at the Data Science Club's DataVerse Conclave! Over three days, we'll fuse minds, ignite creativity, and shape the future of data-driven solutions. Welcome to DataVerse Conclave, where minds converge to revolutionize the landscape of data-driven solutions. Join us on a transformative journey as we fuse ideas, ignite innovation, and shape the future of data science.</p>
