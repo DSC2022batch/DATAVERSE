1dc489decdd51bfe78d454f3486972505ace26d8
Sharad Jadhav <60263979+jadhavsharad@users.noreply.github.com>
2024-02-23 15:29:55 +0530

    updated

--- src/utils/Motion.tsx
+++ src/utils/Motion.tsx
@@ -5,9 +5,11 @@ import { motion, useScroll } from "framer-motion"
 
 const Motion = () => {
     const { scrollYProgress } = useScroll();
+    const scaleX = useSpring(scrollYProgress)
+
     return (
         <div >
-            <motion.div className=" max-w-screen-2xl w-full fixed h-1 saturate-200 bg-rose-900 top-12 z-[999999]" style={{ scaleX: scrollYProgress }} />
+            <motion.div className=" w-full fixed h-1 saturate-200 bg-rose-900 top-12 z-[999999]" style={{ scaleX }} />
         </div>
     )
 }
